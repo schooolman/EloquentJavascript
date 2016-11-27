2a55c63bdc444ce0a3a1cf3102decf19347ef6e2
Jake Schoolmeesters <j.schoolmee@gmail.com>
2016-11-26 15:57:39 -1000

    finished the chapter will start on the exercises tomorrow.

--- Practice_Files/ChapThree_Practice/scripts.js
+++ Practice_Files/ChapThree_Practice/scripts.js
@@ -36,3 +36,15 @@ function greet(who) {
 greet("harry");
 console.log("Bye Felicia");
 //------------------------------------
+
+function power(base, exponent) {
+  if(exponent === 0)
+    return 1;
+  else
+    return base * power(base, exponent -1);
+}
+
+console.log(power(2, 3));
+
+
+//------------------------------------
