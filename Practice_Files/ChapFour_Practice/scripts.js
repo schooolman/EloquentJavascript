aa79ecca3f4be5e4c94cdd9ad1361ce8c367b766
Jake Schoolmeesters <j.schoolmee@gmail.com>
2016-11-29 10:06:13 -1000

    Trying to work through an apparently outdated tutorial on using scroll magic. Ugh. Need to bring requireJS into this project to includ GSAP, which is severely annoying.

--- Practice_Files/ChapFour_Practice/scripts.js
+++ Practice_Files/ChapFour_Practice/scripts.js
@@ -1,3 +1,26 @@
 // -------------------------------------------- //
 // ---------Chpater Four examples---------------//
 // -------------------------------------------- //
+$(function() {
+
+  // Init Controller
+  var scrollMagicController = new ScrollMagic.Controller();
+
+  //creating the animation that will trigger on certain "scene" moments
+  //according to the scrollMagic plugin.
+  var tween = TweenMax.to("#animation", 0.5, {
+    backgroundColor: 'rgb(255, 39, 46)',
+    scale: 7,
+    rotation: 360
+
+  });
+
+  // Create the Scene and trigger when visible with ScrollMagic
+  var scene = new ScrollMagic.Scene({
+    triggerElement: '#scene',
+    offset: 150 // offset the trigger 150px below #scene's top
+  })
+  .setTween(tween)
+  .addTo(scrollMagicController);
+
+});
